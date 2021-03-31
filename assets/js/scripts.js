c49183d631e62b4c735b29b72e4524c141d13a3d
Raúl Jiménez Ortega <hhkaos@gmail.com>
2021-03-31 09:49:20 +0200

    Adding logos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,7 @@
     */
     (function($) {
     "use strict"; // Start of use strict
-  
+
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -19,7 +19,7 @@
         }
       }
     });
-  
+
     // Scroll to top button appear
     $(document).scroll(function() {
       var scrollDistance = $(this).scrollTop();
@@ -29,18 +29,18 @@
         $('.scroll-to-top').fadeOut();
       }
     });
-  
+
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function() {
       $('.navbar-collapse').collapse('hide');
     });
-  
+
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
       target: '#mainNav',
       offset: 80
     });
-  
+
     // Collapse Navbar
     var navbarCollapse = function() {
       if ($("#mainNav").offset().top > 100) {
@@ -53,7 +53,7 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
-  
+
     // Floating label headings for the contact form
     $(function() {
       $("body").on("input propertychange", ".floating-label-form-group", function(e) {
@@ -64,6 +64,48 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
-  
+
+
+
+    /*
+
+  */
+
+  $('.sponsor-grid__logo').on('click', function(){
+
+    if($(this).next('.sponsor-grid__expander').hasClass("active")){
+        //closes other open items
+      $('.sponsor-grid__expander').removeClass('active');
+      $('.sponsor-grid__item').css( "margin-bottom", 0 );
+      $('.icon').removeClass('rotate-arrow');
+      $('.sponsor-grid__logo').removeClass('sponsor-background--color');
+    }
+    else{
+        $('.sponsor-grid__logo').removeClass('sponsor-background--color');
+        $(this).addClass('sponsor-background--color');
+        //delays playing animation while another item is closing
+        if($('.sponsor-grid__expander').hasClass('active')){
+            $(this).parent().addClass('delay');
+            $(this).next('.sponsor-grid__expander').addClass('delay');
+        }
+        $('.sponsor-grid__expander').removeClass('active');
+        $('.icon').removeClass('rotate-arrow');
+        $('.icon', this).addClass('rotate-arrow');
+        $('.sponsor-grid__item').css( "margin-bottom", 0 );
+        //checks if the animation has ended and removes the delay
+        $(this).next('.sponsor-grid__expander').addClass('active').one("transitionend", function(){
+            $('.sponsor-grid__expander').removeClass('delay');
+        });
+
+        var $height = $(this).next('.sponsor-grid__expander').height();
+        //pushes down the row below and removes delay class
+        $(this).parent().css( "margin-bottom", $height ).one("transitionend", function(){
+            $('.sponsor-grid__item').removeClass('delay');
+        });;
+
+    }
+  });
   })(jQuery); // End of use strict
-  
+
+
+
